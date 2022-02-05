8a9aef149b4f75d36d213351a182b6717cd26973
vince nguyen <vincehnguyen@ucsb.edu>
2022-02-05 15:40:05 -0800

    Changed api calls to support new backend infrastructure

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import RICIBs from "react-individual-character-input-boxes";
 import { Button, Grid, Paper, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import SendIcon from "@mui/icons-material/Send";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useEventListener from "@use-it/event-listener";
+import axios from "axios";
+import { alignProperty } from "@mui/material/styles/cssUtils";
+import { isValid } from "ipaddr.js";
+
+// const api = axios.create({
+//   baseURL: "/",
+// });
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -13,11 +20,29 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
+
+const endpoint = "http://localhost:5000";
+
 function App() {
   const [word, setWord] = useState("w");
   const [amount, setAmount] = useState(5);
   const [wordList, setWordList] = useState([]);
 
+  useEffect(() => {}, []);
+
+  async function sendWord() {
+    // const json = JSON.stringify({ event: word, data: "among us" });
+    let res = await axios.post(endpoint + "/words", { input: word });
+    // console.log(res.data.isValid);
+    // console.log(typeof parseInt(res.data.isValid));
+    // console.log(res.data.isValid == 1);
+    return parseInt(res.data.isValid);
+  }
+  async function getWord() {
+    let res = await axios.get(endpoint + "/words");
+    console.log(res);
+  }
+  //In the case of enter key being pressed
   function handler({ key }) {
     if (key == "Enter") {
       if (word.length == 5) {
@@ -27,6 +52,10 @@ function App() {
           // Something you want delayed.
         }, 50); // How long you want the delay to be, measured in milliseconds.
         setWordList([...wordList, word]);
+        setTimeout(function () {
+          setWord("");
+          // Something you want delayed.
+        }, 50); // How long you want the delay to be, measured in milliseconds.
         alert(word);
       }
     }
@@ -62,23 +91,36 @@ function App() {
             style={{ marginTop: "40px" }}
             sx={{ size: "large" }}
             variant="contained"
-            onClick={() => {
+            onClick={async () => {
               if (word.length != 5) {
                 alert("Your word must be 5 letters :)");
                 return;
               }
+
+              //send word to backend and check if it is a valid word or not
+              let isValid = await sendWord();
+
+              if (isValid == 1) {
+                setWordList([...wordList, word]);
+                alert(word + " was sucessfully sent!");
+              } else {
+                alert(word + " is a duplicate, try another :)");
+              }
+
+              //clear input
               setAmount(0);
               setTimeout(function () {
                 setAmount(5);
-                // Something you want delayed.
-              }, 50); // How long you want the delay to be, measured in milliseconds.
-              setWordList([...wordList, word]);
-              alert(word);
+              }, 50);
+
+              //Clear word state
+              setWord("");
             }}
             endIcon={<SendIcon />}
           >
             Send
           </Button>
+          {/* <Button onClick={getWord}>get</Button> */}
         </div>
         <div className="Word-bank">
           {" "}
